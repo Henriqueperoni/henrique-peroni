d4b82ab04a710a87c942bde9949c974596762d7b
Henrique <henriqueperoni@hotmail.com>
2021-04-02 07:35:37 +0100

    Update intro animations

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,5 +1,16 @@
-const introAnim = gsap.timeline({defaults: {ease: 'power1.out'}})
+const introAnim = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 introAnim.to(".greeting", { y: "0%", duration: 1, delay: 0.2, stagger: 0.4 });
-introAnim.fromTo(".intro-image", { y: "50%", opacity: 0 }, { y: "0%", opacity: 1, duration: 1}, "=-1");
-introAnim.to(".btn-portfolio", { opacity: 1, duration: .5 }, "=-0.3");
+introAnim.fromTo(
+  ".intro-image",
+  { transform: 'scale(1.5)', opacity: 0 },
+  { transform: 'scale(1)', opacity: 1, duration: 1 },
+  "=-1"
+);
+introAnim.to(".btn-portfolio", { opacity: 1, duration: 0.5 }, "=-0.3");
+introAnim.fromTo(
+  ".path",
+  { strokeDashoffset: 2000, opacity: 0 },
+  { strokeDashoffset: 0, opacity: 1, duration: 0.8 },
+  "=-1.5"
+);

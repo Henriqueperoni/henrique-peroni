42a460c66a85454296d3c3a645fe52dc6a4d099e
Henrique <henriqueperoni@hotmail.com>
2021-04-03 15:29:22 +0100

    Style and add content to project_detail template

--- projects/static/projects/js/projects.js
+++ projects/static/projects/js/projects.js
@@ -1,6 +1,6 @@
-const projectAnim = gsap.timeline({ defaults: { ease: "power1.out" } });
+// const projectAnim = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-projectAnim.from(".projects", { y: 200, duration: 1});
-projectAnim.from("body > div > div:nth-child(1)", { opacity: 0, duration: 2,}, "-=0.5");
-projectAnim.from("body > div > div:nth-child(1) img", { opacity: 0, transform: 'scale(1.5)', duration: 0.8 }, "-=2");
-projectAnim.from("body > div > div:nth-child(1) > div.line", { width: 0, duration: 0.8 }, "-=1.5");
+// projectAnim.from(".projects", { y: 200, duration: 1});
+// projectAnim.from("body > div > div:nth-child(1)", { opacity: 0, duration: 2,}, "-=0.5");
+// projectAnim.from("body > div > div:nth-child(1) img", { opacity: 0, transform: 'scale(1.5)', duration: 0.8 }, "-=2");
+// projectAnim.from("body > div > div:nth-child(1) > div.line", { width: 0, duration: 0.8 }, "-=1.5");

1095e8b0e15fc620c7eaf14f1618071b906e0a50
Henrique <henriqueperoni@hotmail.com>
2021-04-03 17:40:07 +0100

    Change intro animation and fix minor images position bugs on the project app

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,27 +1,31 @@
 const introAnim = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 introAnim.to(".greeting", { y: "0%", duration: 1, delay: 0.2, stagger: 0.4 });
-introAnim.fromTo(
+introAnim.from(
   ".intro-image",
-  { transform: 'scale(1.5)', opacity: 0 },
-  { transform: 'scale(1)', opacity: 1, duration: 1 },
+  { transform: "scale(1.5)", opacity: 0, duration: 1 },
   "=-1"
 );
-introAnim.to(".btn", { opacity: 1, duration: 0.5 }, "=-0.3");
 introAnim.fromTo(
   ".path",
   { strokeDashoffset: 2000, opacity: 0 },
-  { strokeDashoffset: 0, opacity: 1, duration: 0.8 },
-  "=-1.5"
+  { strokeDashoffset: 0, opacity: 1, duration: 1.5 },
+  "=-1.8"
 );
+introAnim.to(".btn", { opacity: 1, duration: 0.5 }, "=-0.3");
 
 const showAbout = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".about-text",
-        start: "top bottom",
-    }
-})
+  scrollTrigger: {
+    trigger: ".about-text",
+    start: "top bottom",
+  },
+});
 
-showAbout.from(".hard-work-wrapper h2", {opacity: 0, x: -250, duration: 1, stagger: 0.3})
-showAbout.from(".about-text", {opacity: 0, x: 100, duration: 1}, "=-1")
-showAbout.from(".path-polygon", {strokeDashoffset: 5500, duration: 4 }, "=-1")
+showAbout.from(".hard-work-wrapper h2", {
+  opacity: 0,
+  x: -250,
+  duration: 1,
+  stagger: 0.3,
+});
+showAbout.from(".about-text", { opacity: 0, x: 100, duration: 1 }, "=-1");
+showAbout.from(".path-polygon", { strokeDashoffset: 5500, duration: 4 }, "=-1");

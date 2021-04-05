a0402ab8fcc5c35f22e23a877c464fdae0ad40f1
Henrique <henriqueperoni@hotmail.com>
2021-04-05 08:27:01 +0100

    Create contact app, add url, view and template to it

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

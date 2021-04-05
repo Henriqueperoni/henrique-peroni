8b7739f1a101e7500c962272261cd323f1d26142
Henrique <henriqueperoni@hotmail.com>
2021-04-05 09:55:31 +0100

    Create Contact model and made migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    """ Model to create Contact Form """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    phone_number = PhoneField(max_length=20, blank=True)
+    message = models.TextField(max_length=1500, null=False, blank=False)
+
+    def __str__(self):
+        return f'Contact form from {self.name}'
+    

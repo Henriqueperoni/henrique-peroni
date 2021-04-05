ecb687b3a7fed689d0c2f1ca362dc325f621f783
Henrique <henriqueperoni@hotmail.com>
2021-04-05 11:52:19 +0100

    Delete phone field from contact model and keep styling the contact form

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,26 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ("__all__")
+        fields = ('name', 'email', 'message')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus no first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Enter your Name',
+            'email': 'Enter you Email',
+            'message': 'How can I help you?',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]}'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-contact'
+            self.fields[field].label = False
--- contact/models.py
+++ contact/models.py
@@ -8,7 +8,6 @@ class Contact(models.Model):
     """ Model to create Contact Form """
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
-    phone_number = PhoneField(max_length=20, blank=True)
     message = models.TextField(max_length=1500, null=False, blank=False)
 
     def __str__(self):

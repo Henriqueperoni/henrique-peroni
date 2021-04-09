cb53a7fcf1120d442fa07bbdcd629f5a85bd3b73
Henrique <henriqueperoni@hotmail.com>
2021-04-09 09:26:24 +0100

    Update views to display all posts and add it to blog template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def blog(request):
     """ A view to return all projects """
+    blog_post = Post.objects.all()
 
-    return render(request, 'blog/blog.html')
+    context = {
+        'blog_post': blog_post
+    }
+    return render(request, 'blog/blog.html', context)

9c58bad0fd050ec617576ecfff35aad769c5d782
Henrique <henriqueperoni@hotmail.com>
2021-04-16 11:34:01 +0100

    Make the navbar responsive

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const nav = document.querySelector(".nav-links")
+    const navLinks = document.querySelectorAll(".nav-links li")
+
+    burger.addEventListener("click", () => {
+
+        // Toggle Navbar
+        nav.classList.toggle("nav-active")
+
+        // Animate Each li
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + .4}s`
+            }
+        })
+
+        // Burger Animation
+        burger.classList.toggle("close-burger")
+
+    });
+
+}
+
+navSlide()

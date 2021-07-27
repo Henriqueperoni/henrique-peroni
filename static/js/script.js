e1fe2abd743e52555311ebb121477baaec8f7360
Henrique <henriqueperoni@hotmail.com>
2021-07-27 09:54:51 +0100

    Add active navigation class based on URL

--- static/js/script.js
+++ static/js/script.js
@@ -40,3 +40,23 @@ if (closeMessageBtn) {
         closeMessage()
     })
 };
+
+// Add Active Navigation Class Based on URL
+// Adapt from https://stackoverflow.com/a/58308441
+(function () {
+    let current = location.pathname.split('/');
+    let aboutItem = document.querySelector('nav > ul > li:nth-child(1) > a')
+
+    if (current.length === 3) {
+        current = current[1]
+    } else {
+        aboutItem.classList.add("nav-item-active")
+    }
+    
+    let menuItems = document.querySelectorAll('.nav-links a');
+    for (var i = 0, len = menuItems.length; i < len; i++) {
+        if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+            menuItems[i].className += "nav-item-active";
+        }
+    }
+})();

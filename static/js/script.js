f1a34dacd01f8836ea204c1c2b063b0305ba124c
Henrique <henriqueperoni@hotmail.com>
2021-08-18 08:32:58 +0100

    Add functionality to navbar close when a nav item is clicked

--- static/js/script.js
+++ static/js/script.js
@@ -1,59 +1,72 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger")
-    const nav = document.querySelector(".nav-links")
-    const navLinks = document.querySelectorAll(".nav-links li")
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
-    burger.addEventListener("click", () => {
-
-        // Toggle Navbar
-        nav.classList.toggle("nav-active")
-
-        // Animate Each li
+    // Animate each li
+    const navLinksAnim = () => {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = ''
+                link.style.animation = "";
             } else {
-                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + .4}s`
+                link.style.animation = `navLinksFade 0.5s ease forwards ${
+                    index / 7 + 0.4
+                }s`;
             }
-        })
+        });
+    };
+
+    burger.addEventListener("click", () => {
+        // Toggle Navbar
+        nav.classList.toggle("nav-active");
+
+        // Animate li
+        navLinksAnim();
 
         // Burger Animation
-        burger.classList.toggle("close-burger")
-
+        burger.classList.toggle("close-burger");
     });
 
-}
+    // Close burguer by clicking any of the navigation bar links
+    for (i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+            burger.classList.toggle("close-burger");
+            navLinksAnim();
+        });
+    }
+};
 
-navSlide()
+navSlide();
 
 // Close message container
 function closeMessage() {
-    const message = document.querySelector(".message-container")
-    message.classList.add('delete-message')
-    console.log('close')
-};
+    const message = document.querySelector(".message-container");
+    message.classList.add("delete-message");
+    console.log("close");
+}
 
-const closeMessageBtn = document.querySelector(".close-message")
+const closeMessageBtn = document.querySelector(".close-message");
 
 if (closeMessageBtn) {
     closeMessageBtn.addEventListener("click", () => {
-        closeMessage()
-    })
-};
+        closeMessage();
+    });
+}
 
 // Add Active Navigation Class Based on URL
 // Adapt from https://stackoverflow.com/a/58308441
 (function () {
-    let current = location.pathname.split('/');
-    let aboutItem = document.querySelector('nav > ul > li:nth-child(1) > a')
+    let current = location.pathname.split("/");
+    let aboutItem = document.querySelector("nav > ul > li:nth-child(1) > a");
 
     if (current.length === 3) {
-        current = current[1]
+        current = current[1];
     } else {
-        aboutItem.classList.add("nav-item-active")
+        aboutItem.classList.add("nav-item-active");
     }
-    
-    let menuItems = document.querySelectorAll('.nav-links a');
+
+    let menuItems = document.querySelectorAll(".nav-links a");
     for (var i = 0, len = menuItems.length; i < len; i++) {
         if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
             menuItems[i].className += "nav-item-active";

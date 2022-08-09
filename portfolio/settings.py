ad8b1e3111045ec50b24947b8e23da3ff3d40b7c
Henrique <henriqueperoni@hotmail.com>
2022-08-09 20:12:40 +0100

    Deploying to statics files to AWS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -191,7 +191,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    # Bucket Config
+    # Bucket Config 
     AWS_STORAGE_BUCKET_NAME = 'henrique-peroni-portfolio'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
